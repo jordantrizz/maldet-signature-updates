4b20d4a3ef63df1a8d8329bb6565b63c867efde7
Jordan Trask <jtrask@gmail.com>
2021-12-21 15:37:33 +0000

    Update on 21/12/2021 15:37:32 +UTC

--- check.sh
+++ check.sh
@@ -1,39 +1,50 @@
 #!/usr/bin/env bash
 DIR=$(pwd)
+DATE=$(date "+%d/%m/%Y %H:%M:%S +%Z")
+
+_echo () {
+	echo $@
+	echo $@ >> $DIR/check.log
+	}
+	
 
 getsigver () {
 	CURSIGVER=$(curl -s https://cdn.rfxn.com/downloads/maldet.sigs.ver)
 }
 
 processsigfile () {
+	_echo " - Downloading archive from https://cdn.rfxn.com/downloads/maldet-sigpack.tgz"
 	curl -O https://cdn.rfxn.com/downloads/maldet-sigpack.tgz
+	_echo " - Extracting archive"
 	tar -zxvf $DIR/maldet-sigpack.tgz
-		
+	_echo " - Committing to git."
+	git commit -am "Update on $DATE"
+	git push		
 }
 
-echo "Starting script in $DIR"
+_echo "Starting script in $DIR on $DATE"
 
 if [ ! -f .cursigver ]; then
-        echo "First run, no .cursigver present"
-        echo "Grabbing signature file"
+        _echo "First run, no .cursigver present"
+        _echo "Grabbing signature file"
         getsigver
-        echo $CURSIGVER > .cursigver
+        _echo $CURSIGVER > .cursigver
         LASTSIGVER=$(cat .cursigver)
 else
 	LASTSIGVER=$(cat .cursigver)
 fi
 
 getsigver
-echo "Current signature version $CURSIGVER"
-echo "Last signature version $LASTSIGVER"
+_echo "Current signature version $CURSIGVER"
+_echo "Last signature version $LASTSIGVER"
 
 if [ $CURSIGVER == $LASTSIGVER ]; then
-	echo "No signature update."
+	_echo "No signature update."
 else
-	echo "Signatures updated."
+	_echo "Signatures updated."
 	processsigfile
 	
 fi
 
-echo "Exiting" 
+_echo "Exiting" 
 
